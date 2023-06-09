3da5430fc053298cc7185703663df3e48c9a70c7
gouthamraj020102 <gouthamraj020102@gmail.com>
2023-06-10 00:10:51 +0530

    Hitting backend to send Email

--- src/app/components/email/email.component.ts
+++ src/app/components/email/email.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { EmailService } from 'src/app/service/email.service';
 
 @Component({
   selector: 'email',
@@ -13,9 +15,33 @@ export class EmailComponent {
     message: ""
   }
 
+  flag=false;
+
+  constructor(private email:EmailService, private snack:MatSnackBar) { }
+
   doSubmitForm() {
     console.log("try to submit form");
     console.log("DATA is:", this.data);
+
+    if(this.data.to=='' || this.data.subject=='' || this.data.message=='') {
+      this.snack.open("Fields cannot be empty", "Ok");
+      return;
+    }
+
+    this.flag=true;
+    this.email.sendEmail(this.data).subscribe(
+      response=> {
+        console.log(response);
+        this.flag=false;
+        this.snack.open("Sent successfully", "Ok");
+      },
+      error=> {
+        console.log(error);
+        this.flag=false;
+        this.snack.open("Error Occured", "Ok");
+      }
+
+    )
   }
 
 }
